50814c47bcd712b4bcb862b1bd22ef4cdc1e8df5
sujilal <37291121+sujilal@users.noreply.github.com>
2020-02-07 19:37:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-import Header from "./components/Header";
+import Main from "./components/Main";
+import "antd/dist/antd.css";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <Main />
     </div>
   );
 }
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return (
-    <div>
-      <h2>Header</h2>
-    </div>
-  );
-}
